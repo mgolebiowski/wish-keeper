9dc8750e494a6b8f8f2fbfc1ca4638105cb785fe
Michal Golebiowski <michal.golabek94@gmail.com>
2021-12-10 22:01:34 +0100

    init project

---
+++ README.md
@@ -0,0 +1,7 @@
+# wish-keeper
+
+Service that allows you to save ideas for presents for your friends and family throughout the year for later use during holidays, birthdays and other occasions.
+
+## Database
+
+![DB diagram](./db.svg)
---
+++ src/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+
+  const server = Hapi.server({
+    port: process.env.PORT || 3000,
+    host: "0.0.0.0"
+  });
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", (err) => {
+
+  console.log(err);
+  process.exit(1);
+});
+
+init();
